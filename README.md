1ac2093a5f3da6f758ca4825301ba81cdcf6c34f
tisontang(唐霜) <tisontang@tencent.com>
2019-07-31 11:22:29 +0800

    update api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # etx
 
-A JS event schedule center.
+A JS event controller center.
 
 ## Install
 
@@ -13,13 +13,13 @@ npm i etx
 ES6:
 
 ```js
-import etx from 'etx'
+import Etx from 'etx'
 ```
 
 CommonJS:
 
 ```js
-const { etx } = require('etx')
+const { Etx } = require('etx')
 ```
 
 AMD:
@@ -28,7 +28,7 @@ AMD:
 <script src="./node_modules/etx/dist/etx.js"></script>
 <script>
 define(function(require, exports, module) {
-  const { etx } = require('etx')
+  const { Etx } = require('etx')
 })
 </script>
 ```
@@ -38,14 +38,14 @@ Normal Browsers:
 ```html
 <script src="./node_modules/etx/dist/etx.js"></script>
 <script>
-const { etx } = window['etx']
+const { Etx } = window['etx']
 </script>
 ```
 
 ## Usage
 
 ```js
-const etx = new etx()
+const etx = new Etx()
 etx.on('my_event', (e, ...args) => {
   //...
 })
@@ -74,47 +74,57 @@ etx.emit('some_event', name, age)
 Callback function parameters:
 
 - e: a object which have some information about current event
-  - origin: event name which passed by `emit`,
-  - target: event name which passed by `on`,
-  - priority: event priority,
+  - target: event name which passed by `emit`,
+  - event: event name which passed by `on`,
   - callback: event callback,
-  - callback_index: event callback index in callbacks,
-  - callbacks_length: callbacks length,
-  - stop: function, to stop run other event callbacks,
-  - passed_args: args from prev callback,
+  - priority: event priority,
+  - preventDefault: function, when invoked, the left callbacks of current event will not run
+  - stopPropagation: fuction, when invoked, callbacks of children's and parents' will not run, not contains roots' callbacks
+  - stopImmediatePropagation: function, preventDefault + stopPropagation, and roots' callbacks will not run
   - stack: code stack, you can use it for debug,
-- other parameters which passed by `emit`
+- ...args: which passed by `emit`
 
 **event name rules**
 
 Use `.` to concat deep path.
 
 ```js
-etx.on('root.child', fn) // the etx which have name begin with 'root.child' will be fired
+etx.on('parent.child', fn)
 
-etx.emit('root', data) // fn invoked
-etx.emit('root.child.subchild', data) // fn not invoked
-
-etx.on('*', fn) // will be fired when any emit occurs
+etx.emit('parent', data) // fn not invoked
+etx.emit('parent.child.subchild', data) // fn invoked
 ```
+
+Use `*` to stand for root binding. All emits will trigger the callback of `*`, unless you call `stopImmediatePropagation` inside one of callbacks.
+
+```js
+etx.on('*', fn)
+```
+
+### off(event, callback)
+
+If you do not pass callback, all callbacks of this event will be removed.
+
+Notice: you should must off etx' callbacks when you do not need them!!!
 
 ### once(event, callback, priority)
 
 The same as `on`, callback will only run once, after it is executed, it will be offed.
 
-### off(event, callback)
-
-If you do not pass callback, all callbacks of this event (containing sub-events) will be removed.
-
-Notice: you should must off etx' callbacks when you do not need them!!!
-
 ### emit(event, ...args)
 
-Trigger callback functions of this event by passing parameters.
+Trigger callback functions of this event by passing arguments.
+Will trigger parents' and roots' callbacks when propagete.
+
+`args` will be received by `on` callback function.
+
+### broadcast(event, ...args)
+
+Like emit, and will trigger children's callbacks when propagate (before propagate to parents).
 
 ### dispatch(event, ...args)
 
-The same as `emit`. It is used to callback async functions and return a promise:
+Like `emit`, but use async callback functions and return a promise:
 
 ```js
 etx.on('evt', async function f1() {})
@@ -130,44 +140,26 @@ For this code block, f2 will run after f1 resolved, f3 is the same will run afte
 
 Notice: callback function can be or not be async function.
 
-### broadcast(event, ...args)
+### despatch(event, ...args)
 
-The same as `dispatch`. It is used to callback async functions and return a promise:
+Like `broadcast` and `dispatch`.
 
 ```js
 etx.on('evt', async function f1() {})
 etx.on('evt', async function f2() {})
 etx.on('evt', async function f3() {})
 
-await etx.broadcast('evt').then(() => { // f1, f2, f3 will run at the same time (in parallel)
+await etx.despatch('evt').then(() => { // f1, f2, f3 will run at the same time (in parallel)
   // ...
 })
 ```
 
-All the callback functions will be run at the same time.
-Only after all callbacks resolved, the callback in then will run. If one of callbacks rejected, it not affect others, but the whole process will be rejected finally.
-`.broadcast` will return a array which contains all results of callbacks.
+Each level (current, propagation, root) callbacks will run in parallel.
+Only after all callbacks resolved, the callback in then will run.
+If one of callbacks rejected, it not affect others, but the whole process will be rejected finally.
 
 Notice: callback function can be or not be async function.
 
 ### destroy()
 
 Destory the instance.
-
-## Passed Arguments
-
-`.emit` and `.dispatch` will return the value of the last callback.
-However, you can get the result of each callback during the pipeline by `e.passed_args`.
-
-```js
-evt.on('data', (e, data) => {
-  console.log(e.passed_args)  // [0, 'a']      <-------------------------+
-  return { a: 'ok' }          // ---------------------+                  |
-})                            //                      |                  |
-evt.on('data', (e, data) => { //                      |                  |
-  console.log(e.passed_args)  // { a: 'ok' }    <-----+                  |
-  return 2                    // -----------------------+                |
-})                            //                        |                |
-let res = await evt.dispatch('data', 0, 'a')  // -------+----------------+
-console.log(res)              // 2    <-----------------+
-```
--- src/etx.js
+++ src/etx.js
@@ -1,32 +1,66 @@
-import { makeCodeStack, makeEventFilter, sortItemsByPriorityDESC, convertToAsyncFunction } from './utils.js'
-
 export class Etx {
   constructor() {
-    this._events = []
+    this._listeners = []
     this._isSilent = false
   }
 
   on(event, callback, priority = 10) {
-    this._events.push({ event, callback, priority })
+    if (Array.isArray(event)) {
+      const events = event
+      events.forEach((event) => {
+        this.on(event, callback, priority)
+      })
+      return this
+    }
+
+    this._listeners.push({ event, callback, priority })
+    sort(this._listeners)
     return this
   }
   once(event, callback, priority = 10) {
-    this._events.push({ event, callback, priority, once: 1 })
+    if (Array.isArray(event)) {
+      const events = event
+      events.forEach((event) => {
+        this.once(event, callback, priority)
+      })
+      return this
+    }
+
+    this._listeners.push({ event, callback, priority, once: 1 })
+    sort(this._listeners)
     return this
   }
 
   off(event, callback) {
-    const events = this._events.filter(makeEventFilter(event))
-    events.forEach((item, i) => {
+    if (Array.isArray(event)) {
+      const events = event
+      events.forEach((event) => {
+        this.off(event, callback, priority)
+      })
+      return this
+    }
+
+    this._listeners.forEach((item, i) => {
       if (item.event === event && (callback === undefined || item.callback === callback)) {
-        this._events.splice(i, 1)
+        this._listeners.splice(i, 1)
       }
     })
     return this
   }
 
-  has(event) {
-    const existing = this._events.find(makeEventFilter(event))
+  hasRegistered(event) {
+    if (Array.isArray(event)) {
+      const events = event
+      for (let i = 0, len = events.length; i < len; i ++) {
+        const event = events[i]
+        if (!this.hasRegistered(event)) {
+          return false
+        }
+      }
+      return true
+    }
+
+    const existing = this._listeners.find(item => item.event === event)
     return !!existing
   }
 
@@ -41,131 +75,360 @@ export class Etx {
       return
     }
 
-    const events = this._events.filter(makeEventFilter(event))
-    const items = sortItemsByPriorityDESC(events)
+    const events = this._listeners.filter(item => item.event === event)
+    const parents = this._listeners.filter(item => event.indexOf(item.event + '.') === 0)
+    const roots = this._listeners.filter(item => item.event === '*')
 
-    let isStoped = false
-    const stop = () => {
-      isStoped = true
-    }
+    let isPreventDefault = false
+    let isStopPropagation = false
+    let isStopImmediatePropagation = false
 
-    let result = args
-    for (let i = 0, len = items.length; i < len; i ++) {
-      if (isStoped) {
-        break
-      }
+    const preventDefault = () => { isPreventDefault = true }
+    const stopPropagation = () => { isStopPropagation = true }
+    const stopImmediatePropagation = () => { isStopImmediatePropagation = true }
 
-      const item = items[i]
+    const callback = (item) => {
       const e = {
-        origin: event,
-        target: item.event,
+        target: event,
+        event: item.event,
         priority: item.priority,
         callback: item.callback,
-        callback_index: i,
-        callbacks_length: len,
-        stop,
-        passed_args: result,
+        preventDefault,
+        stopPropagation,
+        stopImmediatePropagation,
         stack: makeCodeStack(),
       }
-      result = item.callback(e, ...args)
 
       if (item.once) {
         this.off(item.event, item.callback)
       }
+
+      item.callback(e, ...args)
     }
 
-    return result
+    for (let i = 0, len = events.length; i < len; i ++) {
+      if (isPreventDefault) {
+        break
+      }
+
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = events[i]
+      callback(item)
+    }
+
+    for (let i = 0, len = parents.length; i < len; i ++) {
+      if (isStopPropagation) {
+        break
+      }
+
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = parents[i]
+      callback(item)
+    }
+
+    for (let i = 0, len = roots.length; i < len; i ++) {
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = roots[i]
+      callback(item)
+    }
+  }
+
+  broadcast(event, ...args) {
+    if (this._isSilent) {
+      return
+    }
+
+    const events = this._listeners.filter(item => item.event === event)
+    const parents = this._listeners.filter(item => event.indexOf(item.event + '.') === 0)
+    const children = this._listeners.filter(item => item.event.indexOf(event + '.') === 0)
+    const roots = this._listeners.filter(item => item.event === '*')
+
+    let isPreventDefault = false
+    let isStopPropagation = false
+    let isStopImmediatePropagation = false
+
+    const preventDefault = () => { isPreventDefault = true }
+    const stopPropagation = () => { isStopPropagation = true }
+    const stopImmediatePropagation = () => { isStopImmediatePropagation = true }
+
+    const callback = (item) => {
+      const e = {
+        target: event,
+        event: item.event,
+        priority: item.priority,
+        callback: item.callback,
+        preventDefault,
+        stopPropagation,
+        stopImmediatePropagation,
+        stack: makeCodeStack(),
+      }
+
+      if (item.once) {
+        this.off(item.event, item.callback)
+      }
+
+      item.callback(e, ...args)
+    }
+
+    for (let i = 0, len = events.length; i < len; i ++) {
+      if (isPreventDefault) {
+        break
+      }
+
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = events[i]
+      callback(item)
+    }
+
+    const relatives = [...children, ...parents]
+    for (let i = 0, len = relatives.length; i < len; i ++) {
+      if (isStopPropagation) {
+        break
+      }
+
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = relatives[i]
+      callback(item)
+    }
+
+    for (let i = 0, len = roots.length; i < len; i ++) {
+      if (isStopImmediatePropagation) {
+        break
+      }
+
+      const item = roots[i]
+      callback(item)
+    }
   }
 
   // in series
   dispatch(event, ...args) {
     return new Promise((resolve, reject) => {
-      const events = this._events.filter(makeEventFilter(event))
-      const items = sortItemsByPriorityDESC(events)
-
-      let i = 0
-      const len = items.length
-      let isStoped = false
-      const stop = () => {
-        isStoped = true
+      if (this._isSilent) {
+        return resolve()
       }
 
-      const through = (params) => {
-        if (isStoped) {
-          reject()
-          return
-        }
+      const events = this._listeners.filter(item => item.event === event)
+      const parents = this._listeners.filter(item => event.indexOf(item.event + '.') === 0)
+      const roots = this._listeners.filter(item => item.event === '*')
 
-        const item = items[i]
-        if (i === len) {
-          resolve(params)
-          return
-        }
+      let isPreventDefault = false
+      let isStopPropagation = false
+      let isStopImmediatePropagation = false
 
+      const preventDefault = () => { isPreventDefault = true }
+      const stopPropagation = () => { isStopPropagation = true }
+      const stopImmediatePropagation = () => { isStopImmediatePropagation = true }
+
+      const callback = (item) => {
         const e = {
-          origin: event,
-          target: item.event,
+          target: event,
+          event: item.event,
           priority: item.priority,
           callback: item.callback,
-          callback_index: i,
-          callbacks_length: len,
-          stop,
-          passed_args: params,
+          preventDefault,
+          stopPropagation,
+          stopImmediatePropagation,
           stack: makeCodeStack(),
         }
-        const fn = convertToAsyncFunction(item.callback)
+        const fn = toAsync(item.callback)
 
         if (item.once) {
           this.off(item.event, item.callback)
         }
 
-        i ++
-        fn(e, ...args).then(through).catch(reject)
+        return fn(e, ...args)
       }
-      through(args)
+
+      const throughEvents = () => new Promise((resolve, reject) => {
+        let i = 0
+        const len = events.length
+
+        const through = () => {
+          if (isPreventDefault) {
+            resolve()
+            return
+          }
+
+          if (isStopImmediatePropagation) {
+            resolve()
+            return
+          }
+
+          const item = events[i]
+          if (i === len) {
+            resolve()
+            return
+          }
+
+          const defer = callback(item)
+          i ++
+
+          return defer.then(through).catch(reject)
+        }
+        through()
+      })
+      const throughParents = () => new Promise((resolve, reject) => {
+        let i = 0
+        const len = parents.length
+
+        const through = () => {
+          if (isStopPropagation) {
+            resolve()
+            return
+          }
+
+          if (isStopImmediatePropagation) {
+            resolve()
+            return
+          }
+
+          const item = parents[i]
+          if (i === len) {
+            resolve()
+            return
+          }
+
+          const defer = callback(item)
+          i ++
+
+          return defer.then(through).catch(reject)
+        }
+        through()
+      })
+      const throughRoots = () => new Promise((resolve, reject) => {
+        let i = 0
+        const len = roots.length
+
+        const through = () => {
+          if (isStopImmediatePropagation) {
+            resolve()
+            return
+          }
+
+          const item = roots[i]
+          if (i === len) {
+            resolve()
+            return
+          }
+
+          const defer = callback(item)
+          i ++
+
+          return defer.then(through).catch(reject)
+        }
+        through()
+      })
+
+      throughEvents().then(throughParents).then(throughRoots).then(resolve).catch(reject)
     })
   }
 
   // in parallel
-  broadcast(event, ...args) {
+  despatch(event, ...args) {
     return new Promise((resolve, reject) => {
-      const events = this._events.filter(makeEventFilter(event))
-      const items = sortItemsByPriorityDESC(events)
-
-      let isStoped = false
-      const stop = () => {
-        isStoped = true
+      if (this._isSilent) {
+        return resolve()
       }
 
-      const promises = []
-      for (let i = 0, len = items.length; i < len; i ++) {
-        if (isStoped) {
-          reject()
-          return
-        }
+      const events = this._listeners.filter(item => item.event === event)
+      const parents = this._listeners.filter(item => event.indexOf(item.event + '.') === 0)
+      const roots = this._listeners.filter(item => item.event === '*')
+      const children = this._listeners.filter(item => item.event.indexOf(event + '.') === 0)
 
-        const item = items[i]
+      let isPreventDefault = false
+      let isStopPropagation = false
+      let isStopImmediatePropagation = false
+
+      const preventDefault = () => { isPreventDefault = true }
+      const stopPropagation = () => { isStopPropagation = true }
+      const stopImmediatePropagation = () => { isStopImmediatePropagation = true }
+
+      const callback = (item) => {
         const e = {
-          origin: event,
-          target: item.event,
+          target: event,
+          event: item.event,
           priority: item.priority,
           callback: item.callback,
-          callback_index: i,
-          callbacks_length: len,
-          stop,
-          passed_args: args,
+          preventDefault,
+          stopPropagation,
+          stopImmediatePropagation,
           stack: makeCodeStack(),
         }
-        const fn = convertToAsyncFunction(item.callback)
-        const defer = fn(e, ...args)
-        promises.push(defer)
+        const fn = toAsync(item.callback)
 
         if (item.once) {
           this.off(item.event, item.callback)
         }
+
+        return fn(e, ...args)
       }
 
-      Promise.all(promises).then(resolve).catch(reject)
+      const broadcastEvents = () => {
+        const promises = []
+        for (let i = 0, len = events.length; i < len; i ++) {
+          if (isPreventDefault) {
+            break
+          }
+
+          if (isStopImmediatePropagation) {
+            break
+          }
+
+          const item = events[i]
+          const defer = callback(item)
+          promises.push(defer)
+        }
+        return Promise.all(promises)
+      }
+      const broadcastPropagation = () => {
+        const promises = []
+        const relatives = [...children, ...parents]
+        for (let i = 0, len = relatives.length; i < len; i ++) {
+          if (isStopPropagation) {
+            break
+          }
+
+          if (isStopImmediatePropagation) {
+            break
+          }
+
+          const item = relatives[i]
+          const defer = callback(item)
+          promises.push(defer)
+        }
+        return Promise.all(promises)
+      }
+      const broadcastRoots = () => {
+        const promises = []
+        for (let i = 0, len = roots.length; i < len; i ++) {
+          if (isStopImmediatePropagation) {
+            break
+          }
+
+          const item = roots[i]
+          const defer = callback(item)
+          promises.push(defer)
+        }
+        return Promise.all(promises)
+      }
+
+      broadcastEvents().then(broadcastPropagation).then(broadcastRoots).then(resolve).catch(reject)
     })
   }
 
@@ -173,7 +436,45 @@ export class Etx {
     // remove all events from global namespace
     // developers should must do this if they use namespace
     // or they will face memory stack overflow
-    this._events.length = 0
+    this._listeners.length = 0
   }
 }
+
 export default Etx
+
+//// utils ////
+
+function sort(items) {
+  items.sort((a, b) => {
+    if (a.priority > b.priority) {
+      return -1
+    }
+    else if (a.priority < b.priority) {
+      return 1
+    }
+    else {
+      return 0
+    }
+  })
+}
+
+function makeCodeStack() {
+  const e = new Error()
+  const stack = e.stack || e.stacktrace
+  const stacks = stack.split('\n')
+  stacks.shift()
+  stacks.shift()
+  const text = stacks.join('\n')
+  return text
+}
+
+function toAsync(fn) {
+  return  (...args) => {
+    try {
+      return Promise.resolve(fn(...args))
+    }
+    catch (e) {
+      return Promise.reject(e)
+    }
+  }
+}
